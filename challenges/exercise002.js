99778253bac366922cdc9632fe9bcb6a01782f20
Isaías Sales <Isaias.sales.uk@gmail.com>
2021-03-29 01:31:05 +0100

    Implemented full exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,26 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.city === "Manchester";
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  return arr.filter(item => item === 'sheep').length;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return (person.address.postCode[0] === "M" && person.address.city !=="Manchester") ? false : (person.address.postCode[0] === "M") ? true : false;
 }
 
 module.exports = {
